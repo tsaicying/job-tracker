ba281cefc979def16549784c7f22dcf8bffe7a53
Jenny <tasicying@gmail.com>
2026-03-24 15:38:53 -0500

    Add login/logout UI and auth state in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,35 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
-import JobList from "./pages/JobList"
-import AddJob from "./pages/AddJob"
-import EditJob from "./pages/EditJob"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { useState, useEffect} from 'react';
+import JobList from "./pages/JobList";
+import AddJob from "./pages/AddJob";
+import EditJob from "./pages/EditJob";
+import { getMe, logout, loginWithGoogle} from './api/auth';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(()=> {
+    fetchUser()
+  }, []);
+
+  const fetchUser = async () => {
+    const data = await getMe();
+    setUser(data)
+  }
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Job application record</Link>
         {' | '}
         <Link to="/add">Add new record</Link>
+        {' | '}
+        {user ? (
+          <>
+            <span> {user.name} </span>
+            {' '}
+            <button onClick={logout}>logout</button>
+          </>
+        ): (<button onClick={loginWithGoogle}>Use google to login</button>)}
       </nav>
       <Routes>
         <Route path="/" element={<JobList />}></Route>
---
+++ client/src/api/auth.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000/auth';
+
+axios.defaults.withCredentials = true;
+
+export const getMe = async () => {
+    try {
+        const res = await axios.get(`${BASE_URL}/me`);
+        return res.data;
+    } catch {
+        return null
+    }
+}
+
+export const logout = () => {
+    window.location.href = `${BASE_URL}/logout`;
+}
+
+export const loginWithGoogle = () => {
+    window.location.href = `${BASE_URL}/google`
+}
