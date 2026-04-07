aab6cc6b8a11712e59aa1d314cdf16a7290bbd32
Jenny <tasicying@gmail.com>
2026-04-07 14:07:18 -0500

    Update baseURL for frontend to correct bugs

--- client/src/api/gmail.js
+++ client/src/api/gmail.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL 
-  ? `${import.meta.env.VITE_API_URL}/api/gamil`
-  : 'http://localhost:5000/gmail'
+  ? `${import.meta.env.VITE_API_URL}/api/gmail`
+  : 'http://localhost:5000/api/gmail'
 
 export const getEmails = async () => {
     const res = await axios.get(`${BASE_URL}/emails`);
